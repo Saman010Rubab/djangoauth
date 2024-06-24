2cd645458e6fd17f31e421cc95c13f176f3b683b
Saman Rubab <75686596+Saman010Rubab@users.noreply.github.com>
2024-06-24 18:03:45 +0500

    image added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+
+from .models import Product
+
+
+def index(request):
+    products= Product.objects.all()
+    return render(request, "polls/products.html" , {"products": products})
+
+def addproduct(request):
+    # products= Products.objects.all()
+    return HttpResponse("Hello, world. You're at the polls index.")
